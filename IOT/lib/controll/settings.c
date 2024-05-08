d4347f97efe7d55ce4fa9daaf76ce6d2d0ef6ea0
JustAugucis <62718304+JustAugucis@users.noreply.github.com>
2024-05-08 15:46:18 +0200

    (Author: Oskar) Control layer get temperature and humidity
    Added control layer functionality:
    - To handle fetching temperature and humidity information from the dht11 sensor
    - To process temperature and humidity requests
    
    Ps: Moving changes from previous repository to this one
    Co-Authored-By: oskab1 <111986046+oskab1@users.noreply.github.com>

--- IOT/lib/controll/settings.c
+++ IOT/lib/controll/settings.c
@@ -4,11 +4,12 @@
 //Module debug modes
 bool window_debugMode = true;
 bool decoder_debugMode = true;
+bool dht11_debugMode = true;
 bool wifi_controller_debugMode = true;
 
 //Global easilly acessable settings
 char greenhouseId[] = "10"; // greenhouse id
 char *AP_SSID = "augustinas";
 char *AP_PSWD = "asdfghjk";
-char *ServerIP = "192.168.15.41";
+char *ServerIP = "192.168.5.40";
 uint16_t  ServerPort = 23;
