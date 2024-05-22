02666edd982b80d1d25de7205e0d16215a071592
JustAugucis <62718304+JustAugucis@users.noreply.github.com>
2024-05-22 12:16:42 +0200

    Fix : Fixing the fix that fixed debug
    - Reverted debug fix since it actually created more problems, instead fixed printouts
    - New issue, when debug is off, one of three automatic sceduled readings die, (2nd one precisely) but only when two marked debugs are off

--- IOT/lib/application/sceduled_sender.c
+++ IOT/lib/application/sceduled_sender.c
@@ -1,11 +1,13 @@
 #include "sceduled_sender.h"
 #include "light_sensor_controller.h"
 #include "dht11_controller.h"
+//#include "settings.c"
 #include <avr/interrupt.h>
+
 
 static uint8_t* dht11_readings[4];
 
-extern sceduled_sender_debugMode;
+extern bool sceduled_sender_debugMode;
 
 // Task for sending all readings
 void sceduled_sender_TaskSendAllReadings(){ //is interup rutine (from periodic function)
@@ -17,15 +19,15 @@ void sceduled_sender_TaskSendAllReadings(){ //is interup rutine (from periodic f
     sceduled_sender_TaskGetdht11Readings();
     sei(); // making reading uninteruptable routine
         // Requires readings to be taken first
-        sceduled_sender_TaskSendHumidityReading();
+        sceduled_sender_TaskSendHumidityReading(); // Issues when debug is off
         // Requires readings to be taken first
         sceduled_sender_TaskSendTemperatureReading();
     
 
     // Debug prinoutout for sceduled task
-    if(sceduled_sender_debugMode){debug_print("Sceduled sender finished task");}
+    if(sceduled_sender_debugMode){debug_print("Sceduled sender finished task \n");}
 }
-void sceduled_sender_TaskGetdht11Readings(){    // NEEDS FIXING, UNSURE WHY ONLY FIRST VALUE GETS SAVED
+void sceduled_sender_TaskGetdht11Readings(){    
         
         uint8_t* temp_hum_ptr = dht11_controller_get_temperature_humidity();
         
@@ -39,10 +41,6 @@ void sceduled_sender_TaskGetdht11Readings(){    // NEEDS FIXING, UNSURE WHY ONLY
 
 // Tries to send only light reading
 void sceduled_sender_TaskSendLightReading(){
-    //void decoder_send (const char* message
-    //, enum COMMUNICATION_PATTERN_t pattern, int sensor, 
-    // const int *value)
-        
         uint16_t light_value = light_sensor_controller_makeReading();
         decoder_send("", UPD_GID_POST_SEN_VAL, 1, &light_value);
         
@@ -50,18 +48,15 @@ void sceduled_sender_TaskSendLightReading(){
 
 // Tries to send only humidity reading
 void sceduled_sender_TaskSendHumidityReading(){
-    //void decoder_send (const char* message
-    //, enum COMMUNICATION_PATTERN_t pattern, int sensor, 
-    // const int *value)
-        //int humidity = (dht11_controller_get_temperature_humidity())[1]; 
         decoder_send("", UPD_GID_POST_SEN_VAL, 2, &dht11_readings[0]);
-        //aaa
+
+        if(sceduled_sender_debugMode){debug_print("Task sceduled_sender_TaskSendHumidityReading called \n");}
+       
 }
 
 // Tries to send only temperature reading
 void sceduled_sender_TaskSendTemperatureReading(){
-     // to disable interrupt  // the interupts
-        //int temperature = (dht11_controller_get_temperature_humidity())[3]; 
         decoder_send("", UPD_GID_POST_SEN_VAL, 4, &dht11_readings[2]);
-    
+
+        if(sceduled_sender_debugMode){debug_print("Task sceduled_sender_TaskSendTemperatureReading called \n");}
 }
--- IOT/lib/controll/settings.c
+++ IOT/lib/controll/settings.c
@@ -5,16 +5,16 @@
 bool window_debugMode = false;
 bool decoder_debugMode = false;
 bool dht11_debugMode = false;
-bool wifi_controller_debugMode = false;
+bool wifi_controller_debugMode = true; // maybe this one
 bool light_sensor_controller_debugMode = false;
-bool sceduled_sender_debugMode = true;
+bool sceduled_sender_debugMode = true; // this for sure
 
 //Global easilly acessable settings
 char greenhouseId[] = "1"; // greenhouse id
 char *AP_SSID = "augustinas";
 char *AP_PSWD = "asdfghjk";
-char *ServerIP = "154.62.108.77";
-uint16_t  ServerPort = 50000;
+//char *ServerIP = "154.62.108.77";
+//uint16_t  ServerPort = 50000;
 
-//char *ServerIP = "192.168.187.40";
-//uint16_t  ServerPort = 23;
+char *ServerIP = "192.168.187.40";
+uint16_t  ServerPort = 23;
